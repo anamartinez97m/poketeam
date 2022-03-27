2c1406a565c78c149288e2c5cb3be61272bffb24
Ana Martínez <anamartinez97m@gmail.com>
2022-03-27 17:25:25 +0200

    Created collection view

--- poketeam/ViewController.swift
+++ poketeam/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let sectionData = [
+        "😂",
+        "😳",
+        "🤪",
+        "🧐",
+        "🤯",
+        "😇"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: UICollectionViewDataSource methods
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokemonCell", for: indexPath)
+        
+        cell.backgroundView.text = sectionData[indexPath.section][indexPath.row]
+
+        return cell
+    }
 }
 
